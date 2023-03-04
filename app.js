64df59232f8e97b2adf4369666a3dd73ec0138f4
Korab <korab.morina444@gmail.com>
2023-03-04 01:29:42 +0100

    Setting up the sign up page

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const request = require('request');
 
+app.use(bodyParser.urlencoded( {extended: true}));
+app.use(express.static("public"));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/signup.html');
+
+    app.post('/', function(request, response) {
+        var firstName = request.body.fname;
+        var lastName = request.body.lname;
+        var email = request.body.email;
+        console.log(firstName, lastName, email)
+    })
+})
 
 app.listen(3000, function() {
     console.log('Server is running on port 3000')
